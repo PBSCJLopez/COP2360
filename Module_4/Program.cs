50924578fe6b9dd4de76d0034431865e19c9f3f1
PBSCJLopez <lopezjose120575@gmail.com>
2026-03-01 18:47:45 -0500

    Module 4

---
+++ Module_4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Module5 {     
+    class Program
+    {
+        static int division(int a, int b)
+        {
+            return a / b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            string input1 = Console.ReadLine();
+
+            Console.WriteLine("Enter second number:");
+            string input2 = Console.ReadLine();
+            try
+            {
+                int num1 = Convert.ToInt32(input1);
+                int num2 = Convert.ToInt32(input2);
+
+                int divisionResult = division(num1, num2);
+                Console.WriteLine("Division of both numbers is:");
+                Console.WriteLine(divisionResult);
+            }
+            catch (FormatException formatex)
+            {
+                Console.WriteLine("Any of the two inputs is not a valid integer.");
+            }
+            catch (DivideByZeroException divideex)
+            {
+                Console.WriteLine("Division by zero is not allowed.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Integer is above limits.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
